e08b6cdaf0943dedc9cd35195dff2df554e6c6b3
sttch <421786612@qq.com>
2023-07-22 14:56:05 +0800

    init

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"app-server/initialize"
 	"app-server/router"
 	"app-server/service"
 	"fmt"
@@ -8,7 +9,7 @@ import (
 
 // 项目运行初始配置
 func init() {
-	//initialize.Init()
+	initialize.Init()
 }
 
 func main() {
