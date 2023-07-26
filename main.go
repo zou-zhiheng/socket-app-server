f22105b85b5a67ec534049d37a6552677aed9834
sttch <421786612@qq.com>
2023-07-26 22:22:30 +0800

    init

--- main.go
+++ main.go
@@ -1,53 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"app-server/initialize"
+	"app-server/router"
+	"app-server/service"
+)
 
-func quicksort(a []int) []int {
-
-	if len(a) < 2 {
-		return a
-	}
-
-	left, right := 0, len(a)-1
-	pivot := right
-	right--
-	for left <= right {
-		if a[left] <= a[pivot] {
-			left++
-			continue
-		}
-
-		if a[right] > a[pivot] {
-			right--
-			continue
-		}
-		a[left], a[right] = a[right], a[left]
-	}
-
-	a[left], a[pivot] = a[pivot], a[left]
-
-	quicksort(a[:left])
-	quicksort(a[left+1:])
-
-	return a
+// 项目运行初始配置
+func init() {
+	initialize.Init()
 }
 
 func main() {
-	nums := []int{6, 3}
-	fmt.Println(quicksort(nums))
+	//开启socket服务
+	go service.SocketServer()
+	engine := router.GetEngine()
+	if err := engine.Run(":7001"); err != nil {
+		panic(err)
+	}
 }
-
-//
-//// 项目运行初始配置
-//func init() {
-//	initialize.Init()
-//}
-//
-//func main() {
-//	//开启socket服务
-//	//go service.SocketServer()
-//	//engine := router.GetEngine()
-//	//if err := engine.Run(":7001"); err != nil {
-//	//	panic(err)
-//	//}
-//}
